e74985bec678d7e06a0973f9ab373a4739ce314a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-02-09 00:10:26 -0500

    Updated main to query user

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <math.h>
 #include <string.h>
 #include "stack.h"
@@ -20,9 +21,43 @@ int main()
 {
 	char eqn[MAX_EQ_LEN];	 // string to store the input RPN expression
 	char resp[MAX_EQ_LEN]; // string to store the user's response "y" or "n"
-
+	bool cont = true;
+	bool check = true;
 	/* TODO */
+	//first time asking for user input
+	printf("Enter and equation (in RPN): \n");
+	fgets(eqn, MAX_EQ_LEN, stdin);
 
+	printf("\n");
+	//while loop for next input(s)
+	while (cont) {
+		check = true;
+		while (check) {
+			//ask for another evaluation
+			printf("Evaluate another? (y/n): ");
+			fgets(resp, MAX_EQ_LEN, stdin);
+			//what to do with user input
+			if (*resp == 'y') {
+				check = false;
+			}
+			else if (*resp == 'n') {
+				break;
+			}
+			else {
+				printf("Invalid input! Try again!\n");
+			}
+
+		}
+		//break out of everything if the user puts in n
+		if(*resp == 'n') {
+			break;
+		}
+		//ask for another equation
+		printf("Enter and equation (in RPN): \n");
+		fgets(eqn, MAX_EQ_LEN, stdin);
+
+		printf("\n");
+	}
 	printf("Exiting...\n");
 	return 0;
 }
