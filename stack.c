6fd3ec526263ad8fe6a1965541830348c86f3857
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-02-10 02:05:13 -0500

    Implemented push function

--- stack.c
+++ stack.c
@@ -3,17 +3,38 @@
 #include <stdlib.h>
 #include "stack.h"
 
+int stack_size = 0;
+Node* head = NULL;
+Node* current = NULL;
+
 int size() {
-    return 0;
+    return stack_size;
 }
 
 void push(double data) {
+    //allocate memory for a new Node struct and store data
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->value = data;
-    newNode->nextNodePointer = NULL;
-    
+    //make the new Node the head if head is NULL
+    if(head == NULL) {
+        head = newNode;
+        current = head;
+        printf("Home: %lf\n", current->value);
+        stack_size++;
+    }
+    //else push it to the top of the stack
+    else {
+        Node* temp = NULL;
+        temp = current->nextNodePointer;
+        current->nextNodePointer = newNode;
+        newNode->nextNodePointer = temp;
+        current = newNode;
+        printf("Next: %lf\n", current -> value);
+        stack_size++;
+    } 
 }
 
 double pop() {
+
     return 0.0;
 }
